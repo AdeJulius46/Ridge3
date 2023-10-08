848f7ad83f64d80f0b168cfcaa3231ccc94a9603
Julius <adegokejulius46@gmail.com>
2023-10-08 16:10:06 +0100

    yh

--- pages/home.js
+++ pages/home.js
@@ -3,13 +3,22 @@ import Image from 'next/image'
 import Group from '../public/Images/Group 22.png'
 const home = () => {
   return (
-    <div className=''>
+    <div className='md:ml-24 md:mr-16'>
 
-    <div className='hidden md:flex  ml-24 mr-16'>
+    <div className='hidden md:flex  '>
 
     <div  className=' ml-[12%] mb-12 '>
       <Image src={Group} />
 
+    </div>
+  </div>
+    <div className='md:hidden    '>
+
+    <div  className='mb-12 flex flex-row justify-center content-center p-8'>
+      <Image src={Group} />
+    </div>
+    <div>
+    <button  className=' text-black    w-full md:w-[280px] h-[51px]  text-[23px]  sha  mt-3  font-bold text-[#001A00] bg-[#FFC655] '>CONTACT US</button>
     </div>
   </div>
 </div>
